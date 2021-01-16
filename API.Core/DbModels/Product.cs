3397f64a148a7f9b887d9714c3690d33ddb8cc98
Ömer Faruk İngeç <49835381+omer1325@users.noreply.github.com>
2021-01-16 08:09:52 +0300

    Proje Mimari Alt Yapısı Kuruldu

--- API.Core/DbModels/Product.cs
+++ API.Core/DbModels/Product.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace API.Data.DbModels
+﻿namespace API.Core.DbModels
 {
     public class Product
     {
--- AngularE-Commerce/Controllers/ProductsController.cs
+++ AngularE-Commerce/Controllers/ProductsController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using API.Data.DataContext;
-using API.Data.DbModels;
-using Microsoft.AspNetCore.Http;
+using API.Core.DbModels;
+using API.Infastructure.DataContext;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
